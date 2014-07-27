59930ea38b938a70fbc4fbd5546912c0ab68552f
Richard Searle <harold.richard.searle@gmail.com>
2014-07-27 08:46:31 -0500

    test against jna testlib

---
+++ src/main/scala/demos/TestLib.scala
@@ -0,0 +1,36 @@
+/* Copyright (c) 20010-2014 Sanjay Dasgupta, All Rights Reserved
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * <p/>
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ */
+
+package demos
+
+import sna.Library
+import com.sun.jna.Pointer
+import com.sun.jna.Native
+
+object TestLib extends App {
+
+  val cLib = Library("/home/rsearle/work/sna/linux-x86-64/libtestlib.so")
+
+  val buf = java.nio.ByteBuffer.allocate(1024).order(java.nio.ByteOrder.nativeOrder());
+  val MAGIC: Byte = 'X'.asInstanceOf[Byte];
+  cLib.fillInt8Buffer(buf, 10, MAGIC)[Int];
+
+  println(buf);
+
+  val bytes = new Array[Byte](10);
+  buf.get(bytes);
+  println(new String(bytes));
+
+  println(buf);
+
+}
